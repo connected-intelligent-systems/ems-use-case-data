52be77bfdf8d8d4f219d28bc1541d2a9278efe6d
Alexander Anisimov <alexander.anisimov@dfki.de>
2025-12-08 18:53:53 +0100

    Added check for existance of intermediate saves.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -9,42 +9,49 @@ import pandas as pd
 from tqdm.notebook import tqdm
 import logging
 
-pio.renderers.default='browser'
+pio.renderers.default = "browser"
 log = logging.getLogger(__name__)
 
 
 def save_intermediate_csv(data, name):
     if cf.save_intermediates:
         path = Path(cf.saving_dir, name + ".csv")
-        log.info(f'Saving data to {path} ...')
+        log.info(f"Saving data to {path} ...")
         data.to_csv(path)
+
+
+def intermediate_exists(name):
+    path = Path(cf.saving_dir, name + ".csv")
+    return path.exists() and path.is_file()
 
 
 def read_intermediate_csv(name, is_device=False):
     path = Path(cf.saving_dir, name + ".csv")
-    log.info(f'Reading data from {path} ...')
+    log.info(f"Reading data from {path} ...")
     if is_device:
         df = pd.read_csv(
-                path,
-                usecols=["_time", "power"],
-                index_col="_time",
-                parse_dates=True,
-                date_format="ISO8601",
-            )
-    else:    
+            path,
+            usecols=["_time", "power"],
+            index_col="_time",
+            parse_dates=True,
+            date_format="ISO8601",
+        )
+    else:
         df = pd.read_csv(
-                path,
-                usecols=["_time", "total"],
-                index_col="_time",
-                parse_dates=True,
-                date_format="ISO8601",
-            )
-    
+            path,
+            usecols=["_time", "total"],
+            index_col="_time",
+            parse_dates=True,
+            date_format="ISO8601",
+        )
+
     return df
 
 
 def load_total_from_clearml(data_root):
-    if not cf.use_cached:
+    if cf.use_cached and intermediate_exists("total_raw"):
+        return read_intermediate_csv("total_raw")
+    else:
         raw_data_path = Path(data_root, cf.total_path_clearml)
 
         data = pd.read_csv(
@@ -59,8 +66,6 @@ def load_total_from_clearml(data_root):
         save_intermediate_csv(data, "total_raw")
 
         return data
-    else:
-        return read_intermediate_csv("total_raw")
 
 
 def process_total(data):
@@ -70,15 +75,14 @@ def process_total(data):
 
 
 def clean_cols(thing_name: str, data: pd.DataFrame):
-    """ CAUTION: Side effects!
-    """
+    """CAUTION: Side effects!"""
     match thing_name:
         case "DECT210 Waschmaschine":
             data.rename(columns={"sensor_160-value": "power"}, inplace=True)
             data.drop(["sensor_163-value"], axis=1, inplace=True)
         case "Smart Switch 6 Spülmaschine":
             data.rename(columns={"sensor_582-value": "power"}, inplace=True)
-    return data  
+    return data
 
 
 def bulk_process_devices(data_root):
@@ -86,25 +90,29 @@ def bulk_process_devices(data_root):
 
     devices = {}
     for thing_name in ts_dirs.keys():
-        device_df = load_device_from_clreaml(data_root, thing_name, clearml_url=ts_dirs[thing_name])
+        device_df = load_device_from_clreaml(
+            data_root, thing_name, clearml_url=ts_dirs[thing_name]
+        )
         processed_device_df = process_device(device_df)
-        save_intermediate_csv(processed_device_df, thing_name + '_processed')
+        save_intermediate_csv(processed_device_df, thing_name + "_processed")
         devices[thing_name] = processed_device_df
 
     return devices
 
 
-def load_device_from_clreaml(data_root, thing_name, clearml_url):   
-    if not cf.use_cached: 
+def load_device_from_clreaml(data_root, thing_name, clearml_url):
+    if cf.use_cached and intermediate_exists(thing_name + "_raw"):
+        return read_intermediate_csv(thing_name + "_raw", is_device=True)
+    else:
         frames = []
-        for p in tqdm(
-            list(clearml_url.iterdir())# + 
-        ):
-            if p.suffix == ".csv" and any([
-                p.stem == "energy",
-                p.stem == "power",
-                p.stem.startswith("sensor"),
-            ]):
+        for p in tqdm(list(clearml_url.iterdir())):  # +
+            if p.suffix == ".csv" and any(
+                [
+                    p.stem == "energy",
+                    p.stem == "power",
+                    p.stem.startswith("sensor"),
+                ]
+            ):
                 df = pd.read_csv(
                     p,
                     usecols=["_time", "_value"],
@@ -117,21 +125,19 @@ def load_device_from_clreaml(data_root, thing_name, clearml_url):
         bulk = pd.concat(frames, axis=1)
         bulk = clean_cols(thing_name, bulk)
 
-        save_intermediate_csv(bulk, thing_name + '_raw')
+        save_intermediate_csv(bulk, thing_name + "_raw")
 
         return bulk
-    else:
-        return read_intermediate_csv(thing_name + '_raw', is_device=True)
 
 
 def process_device(df):
     df.astype(float)
-    df = df.resample(cf.process_freq).max() 
+    df = df.resample(cf.process_freq).max()
     df_process = df.copy()
 
     # Restrict ffill to 1 hour
     bins_per_hour = int(pd.Timedelta("1h") / pd.Timedelta(cf.process_freq))  # 360
-    df_process = df_process.ffill(limit=bins_per_hour) 
+    df_process = df_process.ffill(limit=bins_per_hour)
 
     return df_process
 
@@ -162,14 +168,11 @@ def substract_devices_from_total(total, devices, filter_negatives=True):
 def plot_output(df):
     fig = px.line(df)
     fig.update_layout(height=400)
-    fig.update_layout(legend=dict(
-        title=None,
-        orientation="h",
-        yanchor="bottom",
-        y=1.02,
-        xanchor="right",
-        x=1
-    ))
+    fig.update_layout(
+        legend=dict(
+            title=None, orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1
+        )
+    )
     fig.show()
 
 
@@ -185,8 +188,8 @@ if __name__ == "__main__":
 
     total_processed = process_total(load_total_from_clearml(data_root))
     devices_processed = bulk_process_devices(data_root)
-    baseload_output = substract_devices_from_total(total_processed, devices_processed, filter_negatives=True)
+    baseload_output = substract_devices_from_total(
+        total_processed, devices_processed, filter_negatives=True
+    )
 
     plot_output(baseload_output)
-
-    
