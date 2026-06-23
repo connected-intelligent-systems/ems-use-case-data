a1bff76334f359944985252b6eae5029c1f68a9a
Alexander Anisimov <alexander.anisimov@dfki.de>
2026-06-23 10:26:57 +0200

    Persisting the imputed file, when running the main script.

--- src/config.py
+++ src/config.py
@@ -2,15 +2,16 @@ from pathlib import Path
 
 total_path_clearml = "uri:urn:554b7c6c19254be5/power.csv"
 devices_dict_clearml = {
-        "DECT210 Waschmaschine": "uri:urn:6a112240-117e-4ec3-a129-5bc90908aedb",    
-        "DECT200 Waschmaschine": "uri:urn:4b29c04c920141e8",
-        "Smart Switch 6 Spülmaschine": "uri:urn:e91f9319-71af-4ddb-ab7d-fb47b45d69ad",    
-        "DECT200 Spülmaschine": "uri:urn:cc256ae649904024",
-    }
+    "DECT210 Waschmaschine": "uri:urn:6a112240-117e-4ec3-a129-5bc90908aedb",
+    "DECT200 Waschmaschine": "uri:urn:4b29c04c920141e8",
+    "Smart Switch 6 Spülmaschine": "uri:urn:e91f9319-71af-4ddb-ab7d-fb47b45d69ad",
+    "DECT200 Spülmaschine": "uri:urn:cc256ae649904024",
+}
 
 saving_dir = Path("./intermediate_saves/")
 save_intermediates = True
 use_cached = True
 
-process_freq = '10s'
-output_freq = '15min'
+process_freq = "10s"
+output_freq = "15min"
+# output_freq = "1h"
--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@ import pandas as pd
 
 import config as cf
 from imputation import impute_seasonally
-from preprocessing import intermediate_exists, fetch_and_preprocess, plot_output
+from preprocessing import fetch_and_preprocess, intermediate_exists, plot_output
 
 if __name__ == "__main__":
     pp_file_name = "output"
+    ip_file_prefix = "imputed_base_load_"
     if cf.use_cached and intermediate_exists(pp_file_name):
         path = Path(cf.saving_dir, pp_file_name + ".csv")
         aligned_data = pd.read_csv(
@@ -23,5 +24,7 @@ if __name__ == "__main__":
         raise ValueError("Failed to parse frequency of the preprocessed data.")
     aligned_data = aligned_data[["base"]].asfreq(freq)
 
-    imputed_base_load = impute_seasonally(aligned_data, 2022)
+    year = 2022
+    imputed_base_load = impute_seasonally(aligned_data, year)
+    imputed_base_load.to_csv(Path(cf.saving_dir, ip_file_prefix + str(year) + ".csv"))
     plot_output(imputed_base_load)
